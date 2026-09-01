aec4e24c6caa39e8964f9d0a2d12678dbc14a365
Hammad Ahmed <hammadahmedgfx18@gmail.com>
2026-09-02 00:15:39 +0500

    erros solvedt

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import "react-native-gesture-handler";
 import React from "react";
 import { View, StyleSheet, Platform } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
 import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <SafeAreaProvider style={styles.root}>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics} style={styles.root}>
       <View style={styles.root}>
         <NavigationContainer>
           <AppNavigator />
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    height: Platform.OS === "web" ? "100vh" : "100%",
+    minHeight: Platform.OS === "web" ? "100vh" : "100%",
     width: "100%",
   },
 });
